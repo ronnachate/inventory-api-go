819c3d80ed7f72cfebe705b107a372e75a4070ff
Ronnachate <ronnachate@gmail.com>
2023-10-19 16:25:11 +0700

    Add user model.

---
+++ models/user_status.go
@@ -0,0 +1,6 @@
+package models
+
+type UserStatus struct {
+	ID   uint64 `gorm:"primaryKey"`
+	Name string `gorm:"type:varchar(100);not null"`
+}
---
+++ models/users.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Username  string     `gorm:"type:varchar(50);uniqueIndex;not null"`
+	Title     string     `gorm:"type:varchar(50)"`
+	Name      string     `gorm:"type:varchar(100);not null"`
+	Lastname  string     `gorm:"type:varchar(100)"`
+	StatusID  int        `gorm:"not null"`
+	Status    UserStatus `gorm:"ForeignKey:StatusID"`
+	CreatedAt time.Time  `gorm:"autoCreateTime"`
+	UpdatedAt time.Time  `gorm:"autoUpdateTime"`
+}
