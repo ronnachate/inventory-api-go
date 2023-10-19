880b45114e7e438129ec9b88ae6bbb94b0ef40a1
Ronnachate <ronnachate@gmail.com>
2023-10-19 22:54:06 +0700

    Inject user testcase to user controller.

--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -4,10 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ronnachate/inventory-api-go/domain"
 )
+
+type UserController struct {
+	UserUsecase domain.UserUsecase
+}
 
 // GetUsers gets all existing users.
 func GetUsers(c *gin.Context) {
 	var users []string = []string{"John", "Jane", "Joe"}
 	c.JSON(http.StatusOK, users)
+}
+
+func (uc *UserController) GetUserById(c *gin.Context) {
+	user, err := uc.UserUsecase.GetByID(c, c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
---
+++ domain/error_response.go
@@ -0,0 +1,6 @@
+package domain
+
+// ErrorResponse serializes error response.
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
