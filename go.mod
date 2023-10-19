e6f5e5fd12e24fc9e8de067afa91662dae2438cc
Ronnachate <ronnachate@gmail.com>
2023-10-19 22:28:49 +0700

    Add user repository with getUSerById.

--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -32,4 +37,5 @@ require (
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.5
 )
---
+++ repository/user_repository.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/ronnachate/inventory-api-go/domain"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) domain.UserRepository {
+	return &userRepository{
+		DB: db,
+	}
+}
+
+func (ur *userRepository) GetByID(c context.Context, id string) (domain.User, error) {
+	var user domain.User
+
+	result := ur.DB.First(&user, "id = ?", id)
+	if result.Error != nil {
+		return user, result.Error
+	}
+	return user, nil
+}
